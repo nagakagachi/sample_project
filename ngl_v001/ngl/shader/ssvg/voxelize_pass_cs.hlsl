84ae142b5d04deb019e9a860cb7b5c08480fc0b5
nagakagachi <nagakagachi@gmail.com>
2025-09-27 00:42:20 +0900

    ssvg
    voxelizeのatomic書き込みを近傍で同一チェックして削減する簡易最適化.
    小Tileグループ毎に1Fに1小タイルのみ実行するスキップ最適化.

--- ngl_v001/ngl/shader/ssvg/voxelize_pass_cs.hlsl
+++ ngl_v001/ngl/shader/ssvg/voxelize_pass_cs.hlsl
@@ -18,10 +18,15 @@ Texture2D			TexHardwareDepth;
 SamplerState		SmpHardwareDepth;
 
 
-#define SHARED_WRITE_OPTIMIZE 1
+// Atomic書き込みの競合を減らす最適化.
+#define REDUCE_ATOMIC_WRITE_OPTIMIZE 1
+// ThreadGroupタイル単位でスキップする最適化のグループタイル幅. 1より大きい数値で実行.
+#define THREAD_GROUP_SKIP_OPTIMIZE_GROUP_TILE_WIDTH 4
 
-#if SHARED_WRITE_OPTIMIZE
-    groupshared uint2 shared_obm_bitmask_addr[TILE_WIDTH*TILE_WIDTH];
+
+#if REDUCE_ATOMIC_WRITE_OPTIMIZE
+    #define REDUCE_ATOMIC_WRITE_OPTIMIZE_TILE_WIDTH 4
+    groupshared uint4 shared_obm_bitmask_addr[TILE_WIDTH*TILE_WIDTH];
 #endif
 
 // DepthBufferに対してDispatch.
@@ -40,9 +45,9 @@ void main_cs(
 	const float2 screen_size_f = float2(cb_dispatch_param.tex_hw_depth_size.xy);
 	const float2 screen_uv = (screen_pos_f / screen_size_f);
 
-    #if 1
+    #if 1 < THREAD_GROUP_SKIP_OPTIMIZE_GROUP_TILE_WIDTH
         // 適当なTile単位処理スキップ軽量化.
-        const uint skip_tile_size = 8;// SxS個のタイルグループ毎に1Fに1タイルだけ処理するシンプル軽量化.
+        const uint skip_tile_size = THREAD_GROUP_SKIP_OPTIMIZE_GROUP_TILE_WIDTH;// SxS個のタイルグループ毎に1Fに1タイルだけ処理するシンプル軽量化.
         const uint tile_skip_id_x = gid.x%skip_tile_size;
         const uint tile_skip_id_y = gid.y%skip_tile_size;
 
@@ -66,8 +71,8 @@ void main_cs(
     if(65535.0 <= abs(view_z))
         return;
 
-    #if SHARED_WRITE_OPTIMIZE
-        shared_obm_bitmask_addr[gindex] = uint2(~uint(0), 0);
+    #if REDUCE_ATOMIC_WRITE_OPTIMIZE
+        shared_obm_bitmask_addr[gindex] = uint4(~uint(0), 0, 0, 0);// 初期無効値.
     #endif
         
     // 深度->PixelWorldPosition
@@ -86,10 +91,13 @@ void main_cs(
             // 占有ビットマスク.
             const float3 voxel_coord_frac = frac(voxel_coordf);
             const uint3 voxel_coord_bitmask_pos = uint3(voxel_coord_frac * k_obm_per_voxel_resolution);
-            
-            #if SHARED_WRITE_OPTIMIZE
-                const uint bitmask_pos_linear_index = calc_occupancy_bitmask_cell_linear_index(voxel_coord_bitmask_pos);
-                shared_obm_bitmask_addr[gindex] = uint2(voxel_index, bitmask_pos_linear_index);
+
+            #if REDUCE_ATOMIC_WRITE_OPTIMIZE
+                uint bitmask_u32_offset;
+                uint bitmask_u32_bit_pos;
+                calc_occupancy_bitmask_voxel_inner_bit_info(bitmask_u32_offset, bitmask_u32_bit_pos, voxel_coord_bitmask_pos);
+
+                shared_obm_bitmask_addr[gindex] = uint4(voxel_index, 1, bitmask_u32_offset, (1 << bitmask_u32_bit_pos));
             #else
                 const uint unique_data_addr = obm_voxel_unique_data_addr(voxel_index);
                 const uint obm_addr = obm_voxel_occupancy_bitmask_data_addr(voxel_index);
@@ -107,28 +115,56 @@ void main_cs(
         }
     }
 
-    #if SHARED_WRITE_OPTIMIZE
+    #if REDUCE_ATOMIC_WRITE_OPTIMIZE
+        GroupMemoryBarrierWithGroupSync();
+        // シンプルに小Tile毎に最小インデックスの要素との一致をチェックしてマージと除去をする.
+        // 後段のAtomic操作を減らすことが目的.
+        if(0 == (gtid.x%REDUCE_ATOMIC_WRITE_OPTIMIZE_TILE_WIDTH) && 0 == (gtid.y%REDUCE_ATOMIC_WRITE_OPTIMIZE_TILE_WIDTH))
+        {
+            for(int ix = 0; ix < REDUCE_ATOMIC_WRITE_OPTIMIZE_TILE_WIDTH; ++ix)
+            {
+                for(int iy = 0; iy < REDUCE_ATOMIC_WRITE_OPTIMIZE_TILE_WIDTH; ++iy)
+                {
+                    const uint check_index = (gtid.y + iy)*TILE_WIDTH + (gtid.x + ix);
+                    if(check_index == gindex || check_index >= (TILE_WIDTH*TILE_WIDTH))
+                        continue;
+                    
+                    // CoarseVoxelの一致チェック.
+                    if(shared_obm_bitmask_addr[gindex].x == shared_obm_bitmask_addr[check_index].x)
+                    {
+                        shared_obm_bitmask_addr[gindex].y = 0;// CoarseVoxelの占有ビット書き込み不要にする.
+
+                        // u32オフセットも一致する場合はビットマスクをマージ.
+                        if(shared_obm_bitmask_addr[gindex].z == shared_obm_bitmask_addr[check_index].z)
+                        {
+                            shared_obm_bitmask_addr[gindex].w |= shared_obm_bitmask_addr[check_index].w;
+                            shared_obm_bitmask_addr[check_index].x = ~uint(0);// CoarseVoxelも書き込みu32オフセットも一致するため完全にマージして無効化.
+                        }
+                    }   
+                }
+            }
+        }
+
         GroupMemoryBarrierWithGroupSync();
 
         // shared memからバッファ書き込み解決. ここで重複要素への書き込みをマージしてAtomic操作の衝突を最小化したい.
         const uint voxel_index = shared_obm_bitmask_addr[gindex].x;
-        const uint bitmask_linear_index = shared_obm_bitmask_addr[gindex].y;
+        const uint valid_flag = shared_obm_bitmask_addr[gindex].y;
+        const uint bitmask_u32_offset = shared_obm_bitmask_addr[gindex].z;
+        const uint bitmask_append = shared_obm_bitmask_addr[gindex].w;
         if(~uint(0) != voxel_index)
         {
             const uint unique_data_addr = obm_voxel_unique_data_addr(voxel_index);
             const uint obm_addr = obm_voxel_occupancy_bitmask_data_addr(voxel_index);
-            
-            uint bitmask_u32_offset;
-            uint bitmask_u32_bit_pos;
-            calc_occupancy_bitmask_voxel_inner_bit_info_from_linear_index(bitmask_u32_offset, bitmask_u32_bit_pos, bitmask_linear_index);
         
-            const uint bitmask_append = (1 << bitmask_u32_bit_pos);
             // 詳細ジオメトリを占有ビット書き込み.
             InterlockedOr(RWOccupancyBitmaskVoxel[obm_addr + bitmask_u32_offset], bitmask_append);
 
-            // 占有されていることを示すビットをCoarseVoxelに書き込みしてCoarseTraceで参照できるようにする.
-            // TODO. こちらもTileベースでなるべくAtomic操作数を減らしたい.
-            InterlockedOr(RWOccupancyBitmaskVoxel[unique_data_addr], 1);
+            if(0 != valid_flag)
+            {
+                // 一部でも占有されていることを示すビットをCoarseVoxelに書き込みしてCoarseTraceで参照できるようにする.
+                InterlockedOr(RWOccupancyBitmaskVoxel[unique_data_addr], 1);
+            }
         }
     #endif
 
