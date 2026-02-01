3499180b3a7d7e0192eeb6c1090280435acec7fc
naga <nagakagachi@gmail.com>
2026-02-02 00:36:13 +0900

    ssvg
    ssprobeの配置再選択のリトライを追加.

--- ngl_v001/ngl/shader/ssvg/ss_probe_preupdate_cs.hlsl
+++ ngl_v001/ngl/shader/ssvg/ss_probe_preupdate_cs.hlsl
@@ -47,54 +47,56 @@ void main_cs(
     const int2 ss_probe_tile_id = probe_id;
     const int2 ss_probe_tile_pixel_start = ss_probe_tile_id * SCREEN_SPACE_PROBE_TILE_SIZE;
 
-    const uint frame_rand = hash_uint32_iq(probe_id + (probe_id ^ cb_ssvg.frame_count));
-    const uint2 rand_element_in_tile = uint2(frame_rand * SCREEN_SPACE_PROBE_TILE_SIZE_INV, frame_rand) % SCREEN_SPACE_PROBE_TILE_SIZE;
+    // Tile内探索.
+    uint2 probe_pos_in_tile = uint2(0,0);
+    float probe_depth = 1.0;
+    #if 1
+        // 前回情報
+        const float4 prev_probe_info = RWScreenSpaceProbeTileInfoTex[probe_id];
 
-    // このフレームでのプローブ配置テクセル位置をタイル内ランダム選択.
-    const int2 current_probe_texel_pos = ss_probe_tile_pixel_start + rand_element_in_tile;
-    const float2 current_probe_texel_uv = (float2(current_probe_texel_pos) + float2(0.5, 0.5)) * depth_size_inv;// ピクセル中心への半ピクセルオフセット考慮.
-    
+        uint frame_rand = uint(prev_probe_info.y);
+        // 前回が失敗だった場合, または一定の確率でランダム選択.
+        if(!isValidDepth(prev_probe_info.x) || 0.1 > noise_float_to_float(asfloat(cb_ssvg.frame_count ^ probe_id.x + probe_id.y)))
+        {
+            frame_rand = hash_uint32_iq(probe_id + (cb_ssvg.frame_count ^ probe_id));
+        }
 
-    // プローブの配置テクセルの深度取得.
-    const float d = TexHardwareDepth.Load(int3(current_probe_texel_pos, 0)).r;
-    // Skyのテクセルだった場合は再トライするほうが良いか？検討.
+        // 何回かリトライする.
+        for(int i = 0; i < SCREEN_SPACE_PROBE_TILE_SIZE; ++i)
+        {
+            probe_pos_in_tile = uint2(frame_rand * SCREEN_SPACE_PROBE_TILE_SIZE_INV, frame_rand) % SCREEN_SPACE_PROBE_TILE_SIZE;
+            // このフレームでのプローブ配置テクセル位置をタイル内ランダム選択.
+            const int2 current_probe_texel_pos = ss_probe_tile_pixel_start + probe_pos_in_tile;
+            // プローブの配置テクセルの深度取得.
+            probe_depth = TexHardwareDepth.Load(int3(current_probe_texel_pos, 0)).r;
+            if(isValidDepth(probe_depth))
+                break;
 
+            // 次のセルを試行.
+            frame_rand = hash_uint32_iq(probe_id * frame_rand + (cb_ssvg.frame_count ^ probe_id));
+        }
+    #else
+        const uint frame_rand = hash_uint32_iq(probe_id + (probe_id ^ cb_ssvg.frame_count));
+        probe_pos_in_tile = uint2(frame_rand * SCREEN_SPACE_PROBE_TILE_SIZE_INV, frame_rand) % SCREEN_SPACE_PROBE_TILE_SIZE;
+        // このフレームでのプローブ配置テクセル位置をタイル内ランダム選択.
+        const int2 current_probe_texel_pos = ss_probe_tile_pixel_start + probe_pos_in_tile;
+        // プローブの配置テクセルの深度取得.
+        probe_depth = TexHardwareDepth.Load(int3(current_probe_texel_pos, 0)).r;
+    #endif
 
-    if(isValidDepth(d))
+    if(isValidDepth(probe_depth))
     {
-        // 自己遮蔽回避用のサーフェイス探索レイトレース.
-        const float3 pixel_pos_vs = CalcViewSpacePosition(screen_uv, calc_view_z_from_ndc_z(d, cb_ngl_sceneview.cb_ndc_z_to_view_z_coef), cb_ngl_sceneview.cb_proj_mtx);
+        const float3 pixel_pos_vs = CalcViewSpacePosition(screen_uv, calc_view_z_from_ndc_z(probe_depth, cb_ngl_sceneview.cb_ndc_z_to_view_z_coef), cb_ngl_sceneview.cb_proj_mtx);
         const float3 pixel_pos_ws = mul(cb_ngl_sceneview.cb_view_inv_mtx, float4(pixel_pos_vs, 1.0));
         
-        const float3 sample_ray_origin = pixel_pos_ws;
-        const float3 sample_ray_vec = camera_pos - sample_ray_origin;
-        const float3 sample_ray_dir = normalize(sample_ray_vec);
-
-        #if 0
-            const float trace_distance = 0.5;
-            int hit_voxel_index = -1;
-            float4 debug_ray_info;
-            float4 curr_ray_t_ws = 
-            trace_bbv_inverse_bit
-            (
-                hit_voxel_index, debug_ray_info,
-                sample_ray_origin, sample_ray_dir, trace_distance, 
-                cb_ssvg.bbv.grid_min_pos, cb_ssvg.bbv.cell_size, cb_ssvg.bbv.grid_resolution,
-                cb_ssvg.bbv.grid_toroidal_offset, BitmaskBrickVoxel);
-
-            // Bbvでヒットしなかった場合はサーフェイスまでの距離をそのまま格納.
-            const float surface_hit_distance = (curr_ray_t_ws.x > 0.0)? curr_ray_t_ws.x : trace_distance;
-        #else
-            const float surface_hit_distance = 0.0;
-        #endif
-
         // タイル内のプローブ位置をフラットインデックス化.
-        const int probe_pos_flat_index_in_tile = rand_element_in_tile.y * SCREEN_SPACE_PROBE_TILE_SIZE + rand_element_in_tile.x;
+        const int probe_pos_flat_index_in_tile = probe_pos_in_tile.y * SCREEN_SPACE_PROBE_TILE_SIZE + probe_pos_in_tile.x;
         // 配置できたらその情報を格納.
-        RWScreenSpaceProbeTileInfoTex[probe_id] = float4(d, probe_pos_flat_index_in_tile, 0.0, surface_hit_distance);
+        RWScreenSpaceProbeTileInfoTex[probe_id] = float4(probe_depth, probe_pos_flat_index_in_tile, 0.0, 0.0);
     }
     else
     {
+        RWScreenSpaceProbeTileInfoTex[probe_id].x = 1.0;
         // 配置失敗の場合はBbvレイトレース距離を負数.
         RWScreenSpaceProbeTileInfoTex[probe_id].w = -1.0;
     }
